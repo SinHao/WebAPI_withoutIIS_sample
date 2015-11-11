aa47f82eaa9f84a9388c408709bc9e86e39ce51f
SinHao <sos80065@yahoo.com.tw>
2015-11-11 13:50:17 +0800

    v1

---
+++ WebAPI_withoutIIS_sample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace WebAPI_withoutIIS_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration(new Uri("http://localhost:5050"));
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "{controller}/{action}",
+                new { id = RouteParameter.Optional });
+
+            config.MessageHandlers.Add(new CustomHeaderHandler());
+
+            HttpSelfHostServer server = new HttpSelfHostServer(config);
+            
+            server.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            
+            server.OpenAsync().Wait();
+            Console.Read();
+            server.CloseAsync().Wait();
+        }
+    }
+
+    public class CustomHeaderHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            return base.SendAsync(request, cancellationToken)
+                .ContinueWith((task) =>
+                {
+                    HttpResponseMessage response = task.Result;
+                    response.Headers.Add("Access-Control-Allow-Origin", "*");
+                    return response;
+                });
+        }
+    }
+}
---
+++ WebAPI_withoutIIS_sample/SampleController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+
+namespace WebAPI_withoutIIS_sample
+{
+    public class SampleController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Sample";
+        }
+
+        [HttpPost]
+        public string Post1([FromBody]string value)
+        {
+            return value;
+        }
+
+        [HttpPost]
+        public Model Post2([FromBody]Model value)
+        {
+            return value;
+        }
+    }
+
+    public class Model
+    {
+        public string value1 { get; set; }
+        public string value2 { get; set; }
+    }
+}
